57a66a82bf6e1ab637528bf0c891e5f300c74769
王花 <wanghua@bonc.com.cn>
2019-10-17 23:12:36 +0800

    链表相交

--- src/main/java/com/sdz/linkList/LinkList.java
+++ src/main/java/com/sdz/linkList/LinkList.java
@@ -5,4 +5,44 @@ package com.sdz.linkList;
  * @date : 2019/10/17
  */
 public class LinkList {
+    class Node {
+        private Object value;
+        private Node next;
+
+        public Node(Object value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public static Node getMeetNode(Node head1, Node head2) {
+        Node cur1 = head1;
+        Node cur2 = head2;
+        int n = 0;
+        while (cur1.next != null) {
+            n++;
+            cur1 = cur1.next;
+        }
+
+        while (cur2.next != null) {
+            n--;
+            cur2 = cur2.next;
+        }
+
+        if (cur1 != cur2) {
+            return null;
+        }
+
+        cur1 = n > 0 ? head1 : head2;
+        cur2 = cur1 == head1 ? head2 : head1;
+        n = Math.abs(n);
+        while (n > 0) {
+            cur1 = cur1.next;
+        }
+        while (cur1 != cur2) {
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        return cur1;
+    }
 }
